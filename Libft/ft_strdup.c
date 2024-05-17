8c25c7c3512cee8ccb057a1a61c3d8a299bfc6bc
MaxNgew <maxngewjs@gmail.com>
2024-05-17 18:51:08 +0800

    added calloc and strdup

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jngew <jngew@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 18:26:49 by jngew             #+#    #+#             */
+/*   Updated: 2024/05/17 18:50:23 by jngew            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	x;
+
+	x = 0;
+	while (str[x])
+		x++;
+	return (x);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	x;
+	char	*dest;
+
+	x = 0;
+	dest = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[x])
+	{
+		dest[x] = s1[x];
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+    char *original = "Hello World!";
+    char *duplicate = ft_strdup(original);
+
+    if (duplicate == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    printf("Original: %s\n", original);
+    printf("Duplicate: %s\n", duplicate);
+
+    // Free the allocated memory
+    free(duplicate);
+
+    return 0;
+}
+*/
