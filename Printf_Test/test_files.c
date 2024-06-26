70b37e7843f08cbface5060e21daa76593841793
Jin Siang Ngew <jngew@c2r1s2.42singapore.sg>
2024-06-26 11:03:57 +0800

    updated printftest

--- Printf_Test/test_files.c
+++ Printf_Test/test_files.c
@@ -6,7 +6,7 @@
 /*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:08:52 by jngew             #+#    #+#             */
-/*   Updated: 2024/06/07 16:25:03 by jngew            ###   ########.fr       */
+/*   Updated: 2024/06/26 10:56:40 by jngew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,23 @@ int	main()
 
 	age = 33;
 	x = 2024;
-	ft_printf("My name is %s, and I am %d years old. \n", "Max", age);
+	int test = ft_printf("My name is %s, and I am %d years old. \n", "Max", age);
+	int test1 = printf("eval - My name is %s, and I am %d years old. \n", "MaX", age);
+	printf("total length original = %d\n", test);
+	printf("total length expected = %d\n", test1);
 	ft_printf("The address of the pointer, ptr: %p\n", ptr);
+	printf("eval - The address of the pointer , ptr: %p\n", ptr);
 	ft_printf("The year is: %i\n", x);
+	printf("eval - The year is: %i\n", x);
 	ft_printf("Hello %s!\n", "");
 	ft_printf("Value: %d\n", 0);
 	ft_printf("Null test: %c\n", '\0');
 	ft_printf("100%% \n");
+	printf("%%\n");
 	ft_printf("Hex Lowercase: %x\n", 255);
 	ft_printf("Hex Uppercase: %X\n", 255);
+	printf("eval - Hex Lowercase: %x\n", 255);
+	printf("eval - Hex Uppercase: %X\n", 255);
 	ft_printf("Hex LC with Zero: %x\n", 0);
 	ft_printf("Hex UC with ZeroL %X\n", 0);
 	ft_printf("Unsigned int: %u\n", 4294967295u);
