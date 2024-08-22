4849d2ba2ac2f937ea08c05a549821ae28754b69
MaxNgew <maxngewjs@gmail.com>
2024-08-22 18:43:14 +0800

    updated Minitalk & some other files

---
+++ Minitalk_Test/Libft/ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 22:12:13 by jngew             #+#    #+#             */
+/*   Updated: 2024/05/28 16:42:41 by jngew            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	isspace(int x)
+{
+	if ((x >= 9 && x <= 13) || x == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	results;
+	int	x;
+
+	sign = 1;
+	results = 0;
+	x = 0;
+	while (isspace(str[x]))
+		x++;
+	if (str[x] == 43 && str[x + 1] != '-')
+		x++;
+	if (str[x] == '-')
+	{
+		sign = -1;
+		x++;
+	}
+	while (str[x] >= 48 && str[x] <= 57 && str[x])
+	{
+		results *= 10;
+		results += str[x] - 48;
+		x++;
+	}
+	results *= sign;
+	return (results);
+}
