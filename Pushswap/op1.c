9b9646ed9ddfc4ee07bc4b7607d286fd39e9a448
MaxNgew <maxngewjs@gmail.com>
2024-09-05 18:04:31 +0800

    updated pushswap

--- Pushswap/op1.c
+++ Pushswap/op1.c
@@ -6,12 +6,13 @@
 /*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 17:50:15 by jngew             #+#    #+#             */
-/*   Updated: 2024/09/04 01:54:51 by jngew            ###   ########.fr       */
+/*   Updated: 2024/09/05 17:49:54 by jngew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// sa (swap a): Swap the first 2 elements at the top of struct A
 void	ft_swapa(t_stack **a, int x)
 {
 	t_stack	*temp;
@@ -19,12 +20,40 @@ void	ft_swapa(t_stack **a, int x)
 	if (!*a || !(*a)->next)
 		return ;
 	temp = *a;
-	*a = ft_lstlast(*a);
+	*a = (*a)->next;
+	temp->next = (*a)->next;
 	(*a)->next = temp;
-	*a = tmp->next;
-	tmp->next = NULL;
 	if (x == 0)
-		ft_printf ("ra\n");
+		ft_printf("sa\n");
 }
 
-void	ft_
+// ra (rotate a): First element become last, all shift up by 1.
+void	ft_rotatea(t_stack **a, int x)
+{
+	t_stack *temp;
+
+	if (!*a || !(*a)->next)
+		return ;
+	temp = *a;
+	*a = ft_lstlast(*a);
+	(*a)->next = temp;
+	*a = temp->next;
+	temp->next = NULL;
+	if (x == 0)
+		ft_printf("ra\n");
+}
+
+// pa (push a): Take the first element at the top of B and put it on top of A
+void	ft_pusha(t_stack **a, t_stack **b, int x)
+{
+	t_stack *temp;
+
+	if (!*b)
+		return ;
+	temp = *a;
+	*a = *b;
+	*b = (*b)->next;
+	(*a)->next = temp;
+	if (x == 0)
+		ft_printf("pa\n");
+}
