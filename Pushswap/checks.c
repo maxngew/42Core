453a8fa91cb2fde2a81f70b8cb20f960cc71acb7
MaxNgew <maxngewjs@gmail.com>
2024-09-11 00:52:53 +0800

    updated pushswap

--- Pushswap/checks.c
+++ Pushswap/checks.c
@@ -6,7 +6,7 @@
 /*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 00:29:17 by jngew             #+#    #+#             */
-/*   Updated: 2024/09/10 00:33:02 by jngew            ###   ########.fr       */
+/*   Updated: 2024/09/11 00:51:23 by jngew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,31 @@ int	ft_check_sorted(t_stack	*a)
 		a = a->number;
 	}
 	return (1);
+}
+
+void	ft_alphacheck(char **argv)
+{
+	int	x;
+	int	y;
+
+	x = 1;
+	while (argv[x])
+	{
+		y = 0;
+		while ((argv[x][y]) != '\0')
+		{
+			if (ft_isalnum(argv[x][y]))
+				ft_error ();
+			y++;
+		}
+		x++;
+	}
+}
+
+int	ft_checkargs(char **argv)
+{
+	ft_alphacheck(argv);
+	if (!ft_checkerror(argv, 1, 0))
+		return (false);
+	return (true);
 }
