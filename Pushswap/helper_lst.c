c3697a850e6177255030ee5e3d217c98dbea7573
MaxNgew <maxngewjs@gmail.com>
2024-09-12 16:25:28 +0800

    updated push swap files

--- Pushswap/helper_lst.c
+++ Pushswap/helper_lst.c
@@ -6,7 +6,7 @@
 /*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 23:39:09 by jngew             #+#    #+#             */
-/*   Updated: 2024/09/12 12:41:07 by jngew            ###   ########.fr       */
+/*   Updated: 2024/09/12 16:23:51 by jngew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,66 @@ int	ft_max(t_stack *a)
 		if (a->number > x)
 			x = a->number;
 		a = a->next;
+	}
+	return (x);
+}
+
+int	ft_findindex(t_stack *a, int num)
+{
+	int	x;
+
+	x = 0;
+	while (a->number != num)
+	{
+		x++;
+		a = a->next;
+	}
+	a->index = 0;
+	return (x);
+}
+
+int	ft_findplace_b(t_stack *b, int num_push)
+{
+	int		x;
+	t_stack	*temp;
+
+	x = 1;
+	if (num_push > b->number && num_push < ft_lstlast(b)->number)
+		x = 0;
+	else if (num_push > ft_max(b) || num_push < ft_min(b))
+		x = ft_findindex(b, ft_max(b));
+	else
+	{
+		temp = b->next;
+		while (b->number < num_push || temp->number > num_push)
+		{
+			b = b->next;
+			temp = b->next;
+			x++;
+		}
+	}
+	return (x);
+}
+
+int	ft_findplace_a(t_stack *a, int num_push)
+{
+	int		x;
+	t_stack	*temp;
+
+	x = 1;
+	if (num_push < a->number && num_push > ft_lstlast(a)->number)
+		x = 0;
+	else if (num_push > ft_max(a) || num_push < ft_min(a))
+		x = ft_findindex(a, ft_min(a));
+	else
+	{
+		temp = a->next;
+		while (a->number > num_push || temp->number < num_push)
+		{
+			a = a->next;
+			temp = a->next;
+			x++;
+		}
 	}
 	return (x);
 }
